d1f8edf79333edb722250f7d7ae14a82688c3386
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2022-10-25 13:02:14 +0600

    server data req res fumctionality added

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const allCourse = require('./data/course.json');
+
+
+// categories data response
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+
+
+//singel course data response
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    const selectedCourse = allCourse.find( course => course._id === id)
+    res.send(selectedCourse)
+})
+
+
+// categories course data response
+app.get('/categories/:id', (req, res) =>{
+    const id = req.params.id
+    const selectedCategory = allCourse.filter( course => course.category_id == id);
+    res.send(selectedCategory);
+})
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Bright Education server ranning.')
+});
+
+
+app.listen(port, () => {
+    console.log(`Bright Education Server running on port: `, port)
+})
